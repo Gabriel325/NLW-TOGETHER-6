08a8390483f665c75fa91d3a815b44c361a34802
GabrielGazale <gazalegabriel.gag@gmail.com>
2022-02-27 15:16:02 -0300

    Modal Marcar como Lido e Excluir feito

---
+++ public/scripts/main.js
@@ -0,0 +1,32 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+  //adicionar a escuta
+  button.addEventListener('click', handleClick)
+})
+
+//Quando o botão delete for clicado abre a modal
+const deleteButton = document.querySelectorAll('.actions a.delete')
+deleteButton.forEach(button => {
+  button.addEventListener('click', event => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+  const text = check ? 'Marcar como lido' : 'Excluir'
+
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+  //Abir modal
+  modal.open()
+}
